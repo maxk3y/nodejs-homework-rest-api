2d53a995317639347652cac4fd28d3b9e6e75c14
maxk3y <109315215+maxk3y@users.noreply.github.com>
2023-07-17 17:20:00 +0300

    done

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,18 +1,29 @@
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
+const uuid = require('uuid').v4;
 
 const userRolesEnum = require('../constants/userRolesEnum');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const ImageService = require('../services/imageService');
+const Email = require('../services/emailService');
 
 const signToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
 
 exports.signup = catchAsync(async (req, res) => {
+  const verificationToken = uuid().toString();
+
+  const verificationUrl = `/users/verify/${verificationToken}`;
+
   const newUserData = {
     ...req.body,
     role: userRolesEnum.USER,
+    verificationToken,
   };
+
+  const email = req.body.email;
+
+  await new Email()._send(email, verificationUrl);
 
   const user = await User.findOne({ email: req.body.email });
 
@@ -38,6 +49,8 @@ exports.login = catchAsync(async (req, res) => {
   const user = await User.findOne({ email }).select('+password -__v');
 
   if (!user) throw new AppError(401, 'Not authorized!');
+
+  if (user.verify === false) throw new AppError(401, 'To login first verify your email');
 
   const passwordIsValid = await user.checkPassword(password, user.password);
 
@@ -83,5 +96,19 @@ exports.updateAvatar = catchAsync(async (req, res) => {
 
   res.status(200).json({
     user: updatedUser,
+  });
+});
+
+exports.verificateUser = catchAsync(async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOneAndUpdate(
+    { verificationToken },
+    { verify: true, verificationToken: null }
+  );
+
+  if (!user) throw new AppError(404, 'User not found');
+
+  res.status(200).json({
+    message: 'Verification successful',
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -17,6 +17,14 @@ const userSchema = mongoose.Schema({
     enum: ['starter', 'pro', 'business'],
     default: 'starter',
   },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+  },
   token: String,
   avatarURL: String,
 });
